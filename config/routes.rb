46069c5546481734ff1cbdf543c19c611bd2997c
adrien moison <adrimoison@gmail.com>
2018-04-07 14:50:51 +0200

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'about', to: 'pages#about', as: :about
   get 'contact', to: 'pages#contact', as: :contact
+  get 'home', to: 'pages#home', as: :home
   get 'musics', to: 'musics#index'
 
   root to: 'pages#home'
