889692fdbfa3643201b40ef2caf66b4719c5d2f5
adrien moison <adrimoison@gmail.com>
2018-04-07 15:26:27 +0200

    index

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -1,2 +1,12 @@
 class MusicsController < ApplicationController
+
+  MUSICS = {
+    1 => { track: "I love Paris", author: "Guetta", category: "House" }
+    2 => { track: "Defected in the house", author: "Carl Cox", category: "Techno" }
+  }
+
+  def index
+    @musics = MUSICS
+  end
+
 end
