1794b8ad24ad137db51146408c5f8787fbc3d74a
adrien moison <adrimoison@gmail.com>
2018-04-10 23:20:53 +0200

    add link to and path

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get 'about', to: 'pages#about', as: :about
-  get 'contact', to: 'pages#contact', as: :contactgit
+  get 'contact', to: 'pages#contact', as: :contact
 
   get 'musics', to: 'musics#index'
   post 'musics', to: 'musics#create'
-  get 'musics/:id', to: 'musics#show'
+  get 'musics/:id', to: 'musics#show', as: :music
 
   root to: 'pages#home'
 
