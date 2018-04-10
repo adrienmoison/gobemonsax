aa36eacfaae142829fa4a541d0264ed03eac014f
adrien moison <adrimoison@gmail.com>
2018-04-10 22:38:47 +0200

    add params musicsController

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -2,13 +2,14 @@ class MusicsController < ApplicationController
 
   MUSICS = {
     1 => { track: "I love Paris", author: "Guetta", category: "House" },
-    2 => { track: "kiki", author: "adri", category: "techno" },
+    2 => { track: "kiki", author: "adri", category: "Techno" },
     3 => { track: "I love Paris", author: "hello", category: "House" },
     4 => { track: "I love Paris", author: "bonjour", category: "House" },
   }
 
   def index
-    @musics = MUSICS
+    @musics = MUSICS.select do |id, music|
+      music[:category] == params[:style]
+    end
   end
-
 end
