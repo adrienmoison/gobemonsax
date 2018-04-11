d217d42a8cc372ad780ad41242aaa7dfb11a04be
adrien moison <adrimoison@gmail.com>
2018-04-11 22:48:20 +0200

    seeds done

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -1,25 +1,10 @@
 class MusicsController < ApplicationController
 
-  MUSICS = {
-    1 => { track: "I love Paris", author: "Guetta", category: "House" },
-    2 => { track: "kiki", author: "adri", category: "Techno" },
-    3 => { track: "I love Paris", author: "hello", category: "House" },
-    4 => { track: "I love Paris", author: "bonjour", category: "House" },
-  }
-
   def index
-    if params[:style].blank?
-      @musics = MUSICS
-    else
-      @musics = MUSICS.select do |id, music|
-        music[:category] == params[:style]
-      end
-    end
+    @musics = Music.all
   end
 
   def show
-    id = params[:id].to_i
-    @music = MUSICS[id]
   end
 
   def new
