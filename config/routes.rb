26d0d9a4d9bb67b535c03df57f9d5150ec33dcf2
adrien moison <adrimoison@gmail.com>
2018-04-11 22:28:28 +0200

    CRUD routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,26 @@
 Rails.application.routes.draw do
+
+  # Static pages
   get 'about', to: 'pages#about', as: :about
   get 'contact', to: 'pages#contact', as: :contact
 
-  get 'musics', to: 'musics#index'
+  # Create une musique
+  get 'musics', to: 'musics#new'
   post 'musics', to: 'musics#create'
+
+  # Read toutes les musiques
+  get 'musics', to: 'musics#index'
+  # Read une musique
   get 'musics/:id', to: 'musics#show', as: :music
 
+  # Update une musique
+  get 'musics/:id/edit', to: 'musics#edit'
+  patch 'musics/:id', to: 'musics#update'
+
+  # Delete une musique
+  delete 'musics/:id', to: 'musics#destroy'
+
+  # Homepage
   root to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
