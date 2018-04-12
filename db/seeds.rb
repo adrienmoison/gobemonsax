497849fd8aaee1b8b57f63a733b52a49b509221b
adrien moison <adrimoison@gmail.com>
2018-04-12 21:15:43 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Music.destroy_all
-musics_attributes = [
-{
+Music.create(
   track:    "Summertime",
   artist:   "Kat Edmonson",
   category: "Soul",
-  url:      "https://soundcloud.com/symphony-space/feeling-good-into-summertime")
-},
-{
+  url:      "https://soundcloud.com/symphony-space/feeling-good-into-summertime"
+  )
+Music.create(
   track:    "Everybody Dance (Carlos Vargas Disco Mix)",
   artist:   "Chic",
   category: "Disco",
-  url:      "https://soundcloud.com/djcarlosvargas/free-download-chic-everybody-dance-carlos-vargas-disco-mix")
-},
-{
+  url:      "https://soundcloud.com/djcarlosvargas/free-download-chic-everybody-dance-carlos-vargas-disco-mix"
+)
+Music.create(
   track:    "Da",
   artist:   "Knuckle G",
   category: "House",
-  url:      "https://soundcloud.com/letournedisque/knuckle-g-da-master")
-},
-]
+  url:      "https://soundcloud.com/letournedisque/knuckle-g-da-master"
+)
 
-musics_attributes.each { |params| Music.create!(params)}
+
